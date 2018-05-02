351e954acae63d839c3aa4a0b5ec4b541ac36ae2
Daniel Zheng <daniel.zheng@ispy.se>
2018-05-02 11:01:21 +0200

    added Vec3

--- source/pansar/index.js
+++ source/pansar/index.js
@@ -1,25 +1,26 @@
 const canvas = document.querySelector("canvas");
-const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+const engine = new BABYLON.Engine(canvas, true);
+const Vec3 = BABYLON.Vector3;
 
 window.addEventListener("resize", function () {
   engine.resize();
 });
 
 const scene = new BABYLON.Scene(engine);
-scene.enablePhysics(new BABYLON.Vector3(0, -9.82, 0), new BABYLON.CannonJSPlugin());
+scene.enablePhysics(new Vec3(0, -9.82, 0), new BABYLON.CannonJSPlugin());
 
-const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 2, new BABYLON.Vector3(0, 10, 10), scene);
+const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 2, new Vec3(0, 10, 10), scene);
 camera.attachControl(canvas, true);
 
-const light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(10, 10, 0), scene);
-const light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 10, -10), scene);
+const light1 = new BABYLON.HemisphericLight("light1", new Vec3(10, 10, 0), scene);
+const light2 = new BABYLON.PointLight("light2", new Vec3(0, 10, -10), scene);
 
 var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 100, height: 100 }, scene);
 ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
 
 var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameterX: 1 }, scene);
 sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
-sphere.position = new BABYLON.Vector3(0, 5, 0);
+sphere.position = new Vec3(0, 5, 0);
 
 function loadAssets() {
   return new Promise((resolve, reject) => {
@@ -27,7 +28,7 @@ function loadAssets() {
     var meshTask = assetsManager.addMeshTask("monkey task", "", "", "monkey.babylon");
 
     meshTask.onSuccess = function (task) {
-      task.loadedMeshes[0].position = new BABYLON.Vector3(0, 10, 0.1);
+      task.loadedMeshes[0].position = new Vec3(0, 10, 0.1);
       task.loadedMeshes[0].physicsImpostor = new BABYLON.PhysicsImpostor(task.loadedMeshes[0], BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
       resolve();
     }
